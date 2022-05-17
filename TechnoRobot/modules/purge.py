7da24a8b8b2fd879913fe2e83501b619a19fd710
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-17 07:35:35 +0530

    Update purge.py

--- TechnoRobot/modules/purge.py
+++ TechnoRobot/modules/purge.py
@@ -2,7 +2,7 @@ import time
 from telethon import events
 
 from TechnoRobot import telethn
-from TechnoRobot.modules.helper_funcs.telethn.chatstatus import (
+from TechnoRobot.modules.helper_funcs.chat_status import (
     can_delete_messages,
     user_is_admin,
 )
